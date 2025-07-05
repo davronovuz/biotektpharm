2c094174ae95bc6cd44da7dad3af4df56d1074dd
davronovuz <davronovtatu@gmail.com>
2025-07-05 13:53:44 +0500

    ozgarishlar

--- config/settings.py
+++ config/settings.py
@@ -149,9 +149,9 @@ CORS_ALLOW_HEADERS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Media files (User-uploaded content)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
