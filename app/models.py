826f8d739c5fa3f74a62fd11982ff937d11a1aee
davronovuz <davronovtatu@gmail.com>
2025-07-14 12:41:42 +0500

    ozgarishlar html da

--- app/models.py
+++ app/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class Contact(models.Model):
-    """
-    Model to store contact information.
-    """
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15, blank=True, null=True)
