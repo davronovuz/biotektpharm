6fcfe0c3aadfde3137a1bc2329a03709e253486e
davronovuz <davronovtatu@gmail.com>
2025-09-08 10:16:18 +0500

    dasasdasaasdfwsd

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HeroSection,Contact
+from .models import HeroSection,Contact,FeatureCard
 
 @admin.register(HeroSection)
 class HeroSectionAdmin(admin.ModelAdmin):
@@ -13,6 +13,12 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'message')
 
 
+
+@admin.register(FeatureCard)
+class FeatureCardAdmin(admin.ModelAdmin):
+    list_display = ('title_uz', 'icon_type', 'order', 'is_active')
+    list_editable = ('order', 'is_active')
+    search_fields = ('title_uz','title_ru','title_en')
 
 
 
--- app/models.py
+++ app/models.py
@@ -10,6 +10,48 @@ class HeroSection(models.Model):
         return self.title
 
 
+
+
+
+
+class FeatureCard(models.Model):
+    ICON_FA = 'fa'
+    ICON_IMG = 'img'
+    ICON_CHOICES = [
+        (ICON_FA, 'Font Awesome class'),
+        (ICON_IMG, 'Image/SVG upload'),
+    ]
+
+    title_uz = models.CharField(max_length=120)
+    title_ru = models.CharField(max_length=120, blank=True, default='')
+    title_en = models.CharField(max_length=120, blank=True, default='')
+
+    text_uz = models.TextField()
+    text_ru = models.TextField(blank=True, default='')
+    text_en = models.TextField(blank=True, default='')
+
+    icon_type = models.CharField(max_length=3, choices=ICON_CHOICES, default=ICON_FA)
+    fa_class= models.CharField(max_length=64, blank=True, help_text="Masalan: 'fas fa-bullseye'")
+    icon_image= models.ImageField(upload_to='feature_icons/', blank=True, null=True)
+
+    order= models.PositiveIntegerField(default=0)
+    is_active= models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.title_uz
+
+
+
+
+
+
+
+
+
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
